61234cfa00a36c1363940e9faaa7d3a3fda34ae7
uiandwe <uiandwe@gmail.com>
2016-07-11 00:50:00 +0900

    carousel 스크립트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,19 @@ window.onload = function(){
 			$(".img").hover(function(){
 				// alert("!");
 			})
+
+			$("img.active").mouseover(function(){
+				$img_width = $(this).width();
+				$(this).mousemove(function(e){
+					if(e.pageX >= $img_width/2){
+						console.log("오른쪽");
+					}
+					else{
+						console.log("왼쪽");	
+					}
+					
+				});
+			})
+
 	});
 }
